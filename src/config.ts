c39d65467b4de15e0787ce6bc206e1c6afbc80b8
Curso <curso@cursobot.lan>
2026-03-13 16:42:10 -0300

    chore: update avatar and favicon

--- src/config.ts
+++ src/config.ts
@@ -50,7 +50,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Lorem Ipsum",
 	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	links: [
